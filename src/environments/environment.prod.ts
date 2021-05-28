0475d2dd0d799aa9f65f0afef0b0c078d0cbf2f3
LumiereDuMonde <75869694+LumiereDuMonde@users.noreply.github.com>
2021-05-28 16:39:21 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   nationalAPIURL: 'assets/us.json', 
-  signInURL: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBJqVnwW4N_lXqqpEEMTCXjvcXmzJTSNPE',
-  API_KEY: 'AIzaSyBJqVnwW4N_lXqqpEEMTCXjvcXmzJTSNPE',
+  signInURL: '',
+  API_KEY: '',
   GIPHY_API_KEY: '8fKQgxFhleNT0glKiICA1QzAgU77B94x',
   GIPHY_API_URL_TRENDING: 'https://api.giphy.com/v1/gifs/trending',
   GIPHY_API_URL_SEARCH: 'https://api.giphy.com/v1/gifs/search',
